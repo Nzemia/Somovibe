3176ddad17a02b2485744a02eef22e8df7461577
nzemia <mualukofrank@gmail.com>
2026-03-04 03:17:49 +0300

    fixed image naming, docker prisma config

---
+++ types/animejs.d.ts
@@ -0,0 +1,5 @@
+declare module "animejs" {
+  export function animate(targets: any, params: any): any;
+  export function stagger(value: number, options?: any): any;
+  export function set(targets: any, params: any): any;
+}
