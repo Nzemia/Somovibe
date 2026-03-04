7540108068577446b99867b8a3f850dcffc1d69a
nzemia <mualukofrank@gmail.com>
2026-03-04 18:34:22 +0300

    simplify prisma client ctor to fix build

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -4,20 +4,9 @@ const globalForPrisma = global as unknown as {
     prisma: PrismaClient;
 };
 
-const datasourceUrl = process.env.DATABASE_URL;
-
-if (!datasourceUrl) {
-    throw new Error("DATABASE_URL is not set");
-}
-
 export const prisma =
     globalForPrisma.prisma ||
-    new PrismaClient({
-        datasources: {
-            db: { url: datasourceUrl },
-        },
-        log: process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
-    });
+    new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
     globalForPrisma.prisma = prisma;
