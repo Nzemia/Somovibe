033df322b330a03cc012d5cd51e1c96da5b2accb
nzemia <mualukofrank@gmail.com>
2026-03-04 03:50:29 +0300

    fixed prisma adapter

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,16 +1,19 @@
-import { PrismaClient } from "@/app/generated/prisma/client";
+import { PrismaClient } from "@/app/generated/prisma/client"
 
 const globalForPrisma = global as unknown as {
-    prisma: PrismaClient;
-};
+    prisma: PrismaClient
+}
 
 export const prisma =
     globalForPrisma.prisma ||
     new PrismaClient({
         datasourceUrl: process.env.DATABASE_URL,
-        log: process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
-    });
+        log:
+            process.env.NODE_ENV === "development"
+                ? ["error", "warn"]
+                : ["error"]
+    })
 
 if (process.env.NODE_ENV !== "production") {
-    globalForPrisma.prisma = prisma;
+    globalForPrisma.prisma = prisma
 }
