b9b918b9c33ccdb50728ad362ceef734c9546bd5
nzemia <mualukofrank@gmail.com>
2026-03-04 03:50:10 +0300

    removed prisma adapter

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,19 +1,13 @@
 import { PrismaClient } from "@/app/generated/prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
-import { Pool } from "pg";
 
 const globalForPrisma = global as unknown as {
     prisma: PrismaClient;
 };
 
-// Use pooled connection for queries
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-const adapter = new PrismaPg(pool);
-
 export const prisma =
     globalForPrisma.prisma ||
     new PrismaClient({
-        adapter,
+        datasourceUrl: process.env.DATABASE_URL,
         log: process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
     });
 
