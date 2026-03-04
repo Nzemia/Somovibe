2ff858bb8a11b64f3b1dcfc321f0a2f247b83426
nzemia <mualukofrank@gmail.com>
2026-03-04 03:18:08 +0300

    fixed types for anime

--- types/animejs.d.ts
+++ types/animejs.d.ts
@@ -1,5 +1,8 @@
 declare module "animejs" {
-  export function animate(targets: any, params: any): any;
-  export function stagger(value: number, options?: any): any;
-  export function set(targets: any, params: any): any;
+    export function animate(targets: any, params: any): any
+    export function stagger(
+        value: number,
+        options?: any
+    ): any
+    export function set(targets: any, params: any): any
 }
